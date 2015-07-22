05360243cb03c41a86a1372cf74ad294aa18ecfe
John Martin <john.itvn@gmail.com>
2015-07-22 19:51:33 +0700

    Release 1.0.0

---
+++ src/schema/ValidationException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace johnitvn\jsonquery\schema;
+
+/**
+ * @author John Martin <john.itvn@gmail.com>
+ * @since 1.0.0
+ */
+class ValidationException extends \Exception {}
---
+++ src/schema/Validator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace johnitvn\jsonquery\schema;
+
+/**
+ * @author John Martin <john.itvn@gmail.com>
+ * @since 1.0.0
+ */
+class Validator
+{
+    public $error;
+
+    public function check($data, $model, $lax = false)
+    {
+        $result = true;
+        $this->error = '';
+        $constraints = new Constraints($lax);
+
+        try {
+            $constraints->validate($data, $model->data);
+        } catch (ValidationException $e) {
+            $this->error = $e->getMessage();
+            $result = false;
+        }
+
+        return $result;
+    }
+}
